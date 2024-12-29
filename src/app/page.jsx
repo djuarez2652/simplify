686ca20f5f4c6db3c73a0b2f2b7738d3745694f1
djuarez2652 <djuarez0440@gmail.com>
2024-12-29 17:24:39 -0500

    chore: update redirect uri

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -20,7 +20,7 @@ export default function Home() {
         let url = "https://accounts.spotify.com/authorize";
         const client_id = process.env.NEXT_PUBLIC_CLIENT_ID || "";
         // const redirect_uri = "http://localhost:3000/";
-        const redirect_uri = "https://simplify-9ge6y266i-david-juarezs-projects.vercel.app/";
+        const redirect_uri = "https://simplify-j1cd86aq5-david-juarezs-projects.vercel.app";
         const scope = "user-read-private user-read-email user-library-read playlist-modify-public";
 
         url += "?response_type=token";
